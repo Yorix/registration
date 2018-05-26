de7a8373ffa2c2470916eddd25e9a960ebd38059
Yorix <yorix82@gmail.com>
2018-05-26 14:51:50 +0300

    Initial commit

---
+++ src/com/yorix/registration/Controller.java
@@ -0,0 +1,4 @@
+package com.yorix.registration;
+
+public class Controller {
+}
---
+++ src/com/yorix/registration/Main.java
@@ -0,0 +1,29 @@
+package com.yorix.registration;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("main.fxml"));
+        loader.setResources(ResourceBundle.getBundle("com.yorix.registration.bundles.Locale", new Locale("ru")));
+        Parent root = loader.load();
+        primaryStage.setTitle(loader.getResources().getString("title"));
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
